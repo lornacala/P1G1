890d03c11b687e583dc323ae281c2314a03383f1
lornacala <132412322+lornacala@users.noreply.github.com>
2023-05-19 21:59:27 -0400

    Add files via upload

--- PD CODE/code.js
+++ PD CODE/code.js
@@ -92,4 +92,87 @@ function  retornardigitosN() {
     }
 
 }
+function  existeDig3enN() {
+    var N = parseInt(prompt("ingrese N",9478324))
+    var dig = 0
+    while(N != 0){
+        dig = parseInt(N % 10)
+        if(dig == 3){
+            alert("existe el dig 3")
+        }
+        N = parseInt (N / 10)
+    }
+
+}
+function  existeDig3enN() {
+    var N = parseInt(prompt("ingrese N",9478324))
+    var dig = 0
+    while(N != 0){
+        dig = parseInt(N % 10)
+        if(dig == 3){
+            alert("existe el dig 3")
+        }
+        N = parseInt (N / 10)
+    }
+
+}function  mostrarDigitosimpares() {
+    var N = parseInt(prompt("ingrese N",9478324))
+    var dig = 0
+    while(N != 0){
+        dig = parseInt(N % 10)
+        console.log(dig)
+        N = parseInt (N / 10)
+    }
+
+}
+function  mostrarDigitosimpares() {
+    var N = parseInt(prompt("ingrese N",9478324))
+    var dig = 0
+    while(N != 0){
+        dig = parseInt(N % 10)
+        if (dig % 2 == 1){
+            
+        alert("digitos impar: " + dig)
+        }
+        alert("digitos impar: " + dig)
+        N = parseInt (N / 10)
+    }
+
+}
+function  calculararevolumendecilindro() {
+    var R = parseInt(prompt("ingrese el radio: ",3))
+    var H = parseInt(prompt("ingrese la altura:", 5 ))
+    var area = 2 * Math.PI  * R * (R +H)
+    var volumen = Math. PI * Math. pow(R, 2 * H)
+    alert("area: "+ area +" volumen: " + volumen)
+
+}
+function Mostrarnumeroposnegnet() {
+    var N = parseInt(prompt("ingrese N",10))
+    if(N > 0){
+        alert("es positivo")
+    }
+    if(N < 0){
+            alert("es negativo")
+      
+    }
+    if(N == 0){
+        alert("es neutro")
+
+    }
+}
+
+function MostrarsiAesmenoroB() {
+        var N = parseInt(prompt("ingrese A ",10))
+        var N = parseInt(prompt("ingrese B",5))
+        if(A > B){
+            alert("el menor es: ", + B)
+        }
+        if(B < A){
+            alert("el menor es: ", + A )
+          
+        }
+}
+    
+    
 
